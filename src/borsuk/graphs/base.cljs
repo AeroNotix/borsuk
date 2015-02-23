5419c59ec988dcae6a469df901e859b24918e702
Aaron France <aaron.l.france@gmail.com>
2015-02-24 00:27:37 +0100

    Expose message->map

--- src/borsuk/graphs/base.cljs
+++ src/borsuk/graphs/base.cljs
@@ -1,9 +1,14 @@
 (ns borsuk.graphs.base
   (:require [om-tools.dom :as dom]))
 
+
+(defn message->map [event]
+  (when event
+    (js->clj (js/JSON.parse event))))
+
 (defn parse-event [event]
   (when event
-    (let [{:strs [host service state metric]} (js->clj (js/JSON.parse event))]
+    (let [{:strs [host service state metric]} (message->map event)]
       [host service state metric])))
 
 (defn row-event [row]
